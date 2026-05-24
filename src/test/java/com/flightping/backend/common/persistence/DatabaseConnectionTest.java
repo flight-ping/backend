e1154786d160a7c7aabfa32a3e4c2b7521616cf2
seoyeong-y <ysy2329@gmail.com>
2026-05-24 19:22:56 +0900

    chore: 백엔드 서버 PostgreSQL 연동

---
+++ src/test/java/com/flightping/backend/common/persistence/DatabaseConnectionTest.java
@@ -0,0 +1,22 @@
+package com.flightping.backend.common.persistence;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@SpringBootTest
+class DatabaseConnectionTest {
+
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+
+	@Test
+	void databaseConnectionIsAvailable() {
+		Integer result = jdbcTemplate.queryForObject("select 1", Integer.class);
+
+		assertThat(result).isEqualTo(1);
+	}
+}
