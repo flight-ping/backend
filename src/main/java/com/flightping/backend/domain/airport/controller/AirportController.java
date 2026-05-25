02ef20774311c47aaca2c09ccd38d2e68094eea9
seoyeong-y <ysy2329@gmail.com>
2026-05-25 10:15:47 +0900

    feat: 출발지 목록 조회 API

--- src/main/java/com/flightping/backend/domain/airport/controller/AirportController.java
+++ src/main/java/com/flightping/backend/domain/airport/controller/AirportController.java
@@ -1,6 +1,10 @@
 package com.flightping.backend.domain.airport.controller;
 
+import com.flightping.backend.domain.airport.dto.AirportResponse;
+import com.flightping.backend.domain.airport.service.AirportService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/airports")
 @RequiredArgsConstructor
 public class AirportController {
+
+    private final AirportService airportService;
+
+    @GetMapping
+    public ResponseEntity<AirportResponse> getAirports() {
+        return ResponseEntity.ok(airportService.getAirports());
+    }
 }
--- src/main/java/com/flightping/backend/domain/airport/repository/AirportRepository.java
+++ src/main/java/com/flightping/backend/domain/airport/repository/AirportRepository.java
@@ -3,5 +3,9 @@ package com.flightping.backend.domain.airport.repository;
 import com.flightping.backend.domain.airport.entity.Airport;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AirportRepository extends JpaRepository<Airport, Long> {
+
+    List<Airport> findAllByOrderByCityAsc();
 }
