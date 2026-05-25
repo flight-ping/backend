3455f257e8f94b07f3c3f4f81abd24dfcb4ef961
seoyeong-y <ysy2329@gmail.com>
2026-05-25 19:21:59 +0900

    feat: InterestedRoute 엔티티 필드 추가

--- src/main/java/com/flightping/backend/domain/interested/repository/InterestedRouteRepository.java
+++ src/main/java/com/flightping/backend/domain/interested/repository/InterestedRouteRepository.java
@@ -3,5 +3,9 @@ package com.flightping.backend.domain.interested.repository;
 import com.flightping.backend.domain.interested.entity.InterestedRoute;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface InterestedRouteRepository extends JpaRepository<InterestedRoute, Long> {
+
+    List<InterestedRoute> findByUserId(String userId);
 }
